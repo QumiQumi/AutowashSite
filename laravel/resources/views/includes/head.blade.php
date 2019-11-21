301e187c1150b62031422dc765769105161a75b8
QumiQumi <glebyan1998@gmail.com>
2019-11-21 17:10:44 +0700

    сделал пример роутинга

---
+++ laravel/resources/views/includes/head.blade.php
@@ -0,0 +1,20 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="Saquib" content="Blade">
+<title>Checkout our layout</title>
+<!-- load bootstrap from a cdn -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+@section('scripts')
+
+<!-- Scripts -->
+<script src="{{asset('js/app.js')}}" ></script>
+
+
+<!-- Fonts -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+
+<!-- Styles -->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" />
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
@@ -9,8 +9,24 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get(
+//     '/',
+//     function () {
+//         return view('app');
+//     }
+// );
+
+Route::get(
+    '/',
+    function () {
+        return View::make('pages.home');
+    }
+);
+Route::get(
+    '/contact',
+    function () {
+        return View::make('pages.contact');
+    }
+);

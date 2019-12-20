8591fb80f597d858be3bb54671f286a8a08659af
QumiQumi <glebyan1998@gmail.com>
2019-12-21 00:35:35 +0700

    Форма реистрации

--- laravel/resources/views/includes/head.blade.php
+++ laravel/resources/views/includes/head.blade.php
@@ -8,6 +8,8 @@
 {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
 <script src="{{ asset('js/modernizr.custom.js') }}" defer></script>
 
+
+
 <!-- Styles -->
 <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
 
--- laravel/resources/views/includes/header.blade.php
+++ laravel/resources/views/includes/header.blade.php
@@ -2,11 +2,12 @@
     <!-- top bar -->
     <div class="top-bar">
         <div class="container">
+
             <ul class="top-contacts">
                 <li class="top-unhover">
                     <p><span class="fa fa-phone-square" aria-hidden="true"></span> +7 (800) 555 35 35</p>
-                    <li class="top-hover">
-                        <p><span class="fa fa-envelope" aria-hidden="true"></span> <a href="mailto:support@company.com">bestcarwash@mail.ru</a></p>
+                <li class="top-hover">
+                    <p><span class="fa fa-envelope" aria-hidden="true"></span> <a href="mailto:support@company.com">bestcarwash@mail.ru</a></p>
             </ul>
             <ul class="top-links">
                 <li><a href="#"><i class="fa fa-facebook"></i></a></li>
@@ -14,9 +15,45 @@
                 <li><a href="#"><i class="fa fa-vk"></i></a></li>
                 <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
             </ul>
+
+
+            <!-- Right Side Of Navbar -->
+            <ul class="top-auth">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                         {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right dropdown-menu-my" aria-labelledby="navbarDropdown">
+                            <a class="" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                               <div> {{ __('Logout') }}</div>
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+
             <div class="clearfix"></div>
         </div>
     </div>
+
     <!-- /top bar -->
     <div class="header-top">
         <div class="container">
@@ -41,11 +78,43 @@
                         <li><a class="scroll" href="{{route('news')}}">Статьи</a></li>
                         <li><a class="scroll" href="{{route('comments')}}">Отзывы</a></li>
                     </ul>
+
+                     {{-- <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }} <span class="caret"></span>
+                                    </a>
+
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                    </ul> --}}
+
                 </div>
-                <!-- /.navbar-collapse -->
 
             </nav>
-
 
         </div>
 
